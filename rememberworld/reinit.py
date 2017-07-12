ace9f5774388900973e3030241ff52d922d76b20
winston <670293316@qq.com>
2017-07-12 06:03:34 -0700

    change to python3

--- rememberworld/reinit.py
+++ rememberworld/reinit.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import sqlite3
 import time
 
@@ -23,20 +24,20 @@ def initdict(lines):
     current = 0
     for line in lines:
         if current%10 == 0:
-            print total,current
+            print (total,current)
         parm = line.split(' ')
-        if parm > 1:
+        if len(parm) > 1:
             word = parm[0]
             if not haveword(word):
                 definition=''
                 for i in range(1,len(parm)):
                     definition = definition + parm[i]
             
-                sql = 'insert into dict values ( "{0}" ,"{1}",NULL,"{2}",{3})'.format(word,definition.decode('gbk').encode('utf-8'),1,time.time())
+                sql = 'insert into dict values ( "{0}" ,"{1}",NULL,"{2}",{3})'.format(word,definition,1,time.time())
                 saveword(sql)
                 current += 1
                 continue
-        print line
+        print (line)
             
         
 
--- rememberworld/voice/download.py
+++ rememberworld/voice/download.py
@@ -3,9 +3,9 @@ orda = ord('a')
 ordz=ord('z')
 for i in range(orda,ordz+1):
     url = 'http://images.juren.com/file/20090804/'+chr(i)+'.rar'
-    print url
+    print (url)
     r=requests.get(url)
     if r.status_code == 200:
         with open(url.split(r'/')[-1],'wb') as f:
             f.write(r.content)
-            print 'finish' , url
+            print ('finish' , url)
